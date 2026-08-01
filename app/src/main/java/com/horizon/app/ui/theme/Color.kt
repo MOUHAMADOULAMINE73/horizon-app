ca4bcb714b08ce10212ae65caf779a72308702d9
MOUHAMADOULAMINE73 <lemzinho6@gmail.com>
2026-08-01 17:15:20 +0100

    Create Color.kt

---
+++ app/src/main/java/com/horizon/app/ui/theme/Color.kt
@@ -0,0 +1,14 @@
+package com.horizon.app.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val HorizonBlue = Color(0xFF1B2A4A)
+val HorizonBlueDark = Color(0xFF10182E)
+val HorizonOrange = Color(0xFFFF7A45)
+val HorizonOrangeLight = Color(0xFFFFB088)
+val HorizonBackground = Color(0xFFF7F8FA)
+val HorizonSurface = Color(0xFFFFFFFF)
+val HorizonTextPrimary = Color(0xFF1A1A2E)
+val HorizonTextSecondary = Color(0xFF6B7280)
+val HorizonError = Color(0xFFE5484D)
+val HorizonSuccess = Color(0xFF2FAE60)
